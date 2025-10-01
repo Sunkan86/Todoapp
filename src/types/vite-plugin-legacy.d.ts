4d11b08becc4b534d40f12de73ccf44b13c740ab
JonathanAberg <jonathan.aberg@hotmail.com>
2025-10-01 13:11:27 +0200

    chore(eslint): add node resolver and remove custom vite-plugin-legacy type shim

--- src/types/vite-plugin-legacy.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@vitejs/plugin-legacy" {
-  import { Plugin } from "vite";
-  type LegacyOptions = any;
-  const legacyPlugin: (options?: LegacyOptions) => Plugin;
-  export default legacyPlugin;
-}
