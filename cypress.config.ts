38ae17821c1f445eee9899849b15d5acdaa05140
hamoudilaz <hamoudi_lazkani@hotmail.se>
2025-10-10 21:38:09 +0200

    fix: add baseUrl to cypress config file

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   video: false,
   e2e: {
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // No special node event hooks needed for e2e; server is started externally via scripts
       return config;
